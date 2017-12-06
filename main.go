353cb2885069c5134e76a8acc0e51276d2b141e8
sinmetal <metal.tie@gmail.com>
2017-12-06 14:41:17 +0900

    1min毎に1000件のリクエストを送信するようにした refs #2

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"strings"
 	"time"
 
+	"github.com/google/uuid"
+	"github.com/pkg/errors"
 	"github.com/sinmetal/slog"
 )
 
@@ -25,17 +27,28 @@ type ItemAPIPostResponse struct {
 const vtServerURL = "http://vt-server-service.default.svc.cluster.local:8080"
 
 func main() {
-	for i := 0; i < 1000; i++ {
-		Post()
+	for {
+		for i := 0; i < 1000; i++ {
+			lot := uuid.New().String()
+			i := i
+			go func() {
+				Post(lot, i)
+			}()
+		}
+		time.Sleep(1 * time.Minute)
 	}
 }
 
-func Post() {
+func Post(lot string, index int) error {
 	log := slog.Start(time.Now())
 	defer log.Flush()
 
 	body := ItemAPIPostRequest{
-		Contents: []string{"hello client"},
+		Contents: []string{
+			lot,
+			fmt.Sprintf("%d", index),
+			"hello client",
+		},
 	}
 	b, err := json.Marshal(body)
 	if err != nil {
@@ -53,19 +66,20 @@ func Post() {
 	res, err := client.Do(req)
 	if err != nil {
 		log.Errorf("client.Do err = %s", err.Error())
-		return
+		return errors.Wrap(err, "client.Do err")
 	}
 
 	resBody, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		log.Errorf("request.Body %s", err.Error())
-		return
+		return errors.Wrap(err, "read request.Body")
 	}
 
 	if res.StatusCode != http.StatusOK {
 		log.Errorf("response code = %d, body = %s", res.StatusCode, resBody)
-		return
+		return fmt.Errorf("response code = %d, body = %s", res.StatusCode, resBody)
 	}
 
-	log.Infof("response status = %s", res.Status)
+	log.Infof("index = %d, response status = %s", index, res.Status)
+	return nil
 }
